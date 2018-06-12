b0564322b99b7260e93c13eafcfc0c2de7832f74
confragus <confragus@Wilsons-iMac.local>
2018-06-13 00:48:01 +1000

    chuck joke

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -11,7 +11,7 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
 
-  question = data[Math.floor(Math.random() * 10)];
+  question = data[Math.floor(Math.random() * 72)];
 
   const q1 = document.createElement('p');
   q1.textContent = question.Q1;
@@ -33,26 +33,28 @@ request.onload = function () {
   q5.textContent = question.Q5;
   app.appendChild(q5);
 
-  const a1 = document.createElement('p');
-  a1.textContent = question.A1;
-  app.appendChild(q1);
-  
-  const a2 = document.createElement('p');
-  a2.textContent = question.A2;
-  app.appendChild(q2);
-  
-  const a3 = document.createElement('p');
-  a3.textContent = question.A3;
-  app.appendChild(q3);
-  
-  const a4 = document.createElement('p');
-  a4.textContent = question.A4;
-  app.appendChild(q4);
+  if (question.A1 != "") {
+    const a1 = document.createElement('p');
+    a1.textContent = "A: " + question.A1;
+    app.appendChild(a1);
+    
+    const a2 = document.createElement('p');
+    a2.textContent = "B: " + question.A2;
+    app.appendChild(a2);
+    
+    const a3 = document.createElement('p');
+    a3.textContent = "C: " + question.A3;
+    app.appendChild(a3);
+    
+    const a4 = document.createElement('p');
+    a4.textContent = "D: " + question.A4;
+    app.appendChild(a4);
+  };
 
   const answer = document.createElement('p');
-  answer.textContent = question.Answer;
+  answer.textContent = "Answer: " + question.Answer;
   app.appendChild(answer);
-    
+
 }
 
 request.send();
